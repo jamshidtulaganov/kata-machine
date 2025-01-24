0299c596b31a83a8b9484156cb9f9632ba655041
Jamshid Tulaganov <164010503+jamshidtulaganov@users.noreply.github.com>
2025-01-24 09:38:10 +0500

    feat: solved two crstal_ball problem

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,16 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jump_amount = Math.floor(Math.sqrt(breaks.length));
+    let index = jump_amount;
 
+    for (; index < breaks.length; index += jump_amount) {
+        if (breaks[index]) break;
+    }
+
+    index -= jump_amount;
+
+    for (let j = 0; j < jump_amount && index < breaks.length; ++index, ++j) {
+        if (breaks[index]) return index;
+    }
+
+    return -1;
 }
